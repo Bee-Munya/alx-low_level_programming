2967078d36f60cd96025e12510e282dabbc576c1
Bee-Munya <brightmdzngw@gmail.com>
2023-04-05 18:07:09 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @b: string
+ * Return 0;
+*/
+
+void _print_rev_recursion(char *b)
+{
+	if (*s)
+	{
+		_print_rev_recursion(b + 1);
+		_putchar(*b);
+	}
+}

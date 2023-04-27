b7872af742359eeb260041eeb265a389e6a94343
Bee-Munya <brightmdzngw@gmail.com>
2023-04-27 06:05:37 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_before_main(void) __attribute__ ((constructor));
+
+/**
+* print_before_main - Prints a message before the main function is executed.
+*/
+void print_before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
+int main(void)
+{
+printf("This is the main function.\n");
+return (0);
+}
